6c19349957e3f46c4936b9c99ad74c28af22257e
devadob <admir.obralija@tik.uni-stuttgart.de>
2024-07-26 19:41:39 +0200

    ILIAS 9 major release compatibility.
    - Bump plugin version from 6.0.0 to 7.0.0

--- plugin.php
+++ plugin.php
@@ -4,12 +4,12 @@
 $id = "assviplab";
  
 // code version; must be changed for all code changes
-$version = "6.0.0";
+$version = "7.0.0";
  
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "8.0.0";
-$ilias_max_version = "8.999";
+$ilias_min_version = "9.0.0";
+$ilias_max_version = "9.999.999";
 
 // add the responsible person for the plugin
 $responsible = "Per Pascal Seeland"; 
